5d133c6edf6d057fdba556326963cc45b0a41bfe
Vaibhav-Tayade <vaibhavtayadearun@gmail.com>
2023-06-30 22:46:55 +0530

    Function Of Addition and subtraction added into the demo1.py file

--- demo1.py
+++ demo1.py
@@ -1,1 +1,10 @@
 print ("hello world")
+def add():
+    num1=int(input("Enter the value"))
+    num2=int(input("Enter the value"))
+    add=num1+num2
+    subt=num1-num2
+    print("The Addition =",add)
+    print("Subtraction=",subt)
+
+add()
